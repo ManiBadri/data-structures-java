749e570ba4ba5de0887e15e1c765819e8302dc45
unknown <moneybadri@gmail.com>
2026-03-29 21:19:14 -0400

    added array stacks (needs more testing)

---
+++ stack/ArrayStack.java
@@ -0,0 +1,37 @@
+package stack;
+
+public class ArrayStack<T> {
+
+    private T[] arr;
+    private int top;
+
+    public ArrayStack(int capacity) {
+        arr = (T[]) new Object[capacity];
+        top = -1;
+    }
+
+    public void push(T item){
+        if(top == arr.length)
+            throw new IllegalStateException("Stack full");
+        arr[top+1] = item;
+        top++;
+    }
+
+    public T top(){
+        return arr[top];
+    }
+
+    public T pop(){
+        T temp = arr[top];
+        arr[top] = null;
+        top--;
+        return temp;
+    }
+
+    public void clear(){
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = null;
+        }
+    }
+    
+}
--- stack/test.java
+++ stack/test.java
@@ -9,13 +9,22 @@ public class test {
         stack.push(30);
         stack.push(40);
         stack.push(50);
-
         stack.clear();
 
-        System.out.println("");
-        System.out.print("Top:");
-        System.out.print(stack.top());
-        System.out.println("");
+
+
+
+
+        ArrayStack<Integer> arrStack = new ArrayStack<>(10);
+        arrStack.push(5);
+        arrStack.push(6);
+        arrStack.push(7);
+        arrStack.push(8);
+        arrStack.pop();
+
+        System.out.println();
+        System.out.print(arrStack.top());
+        System.out.println();
         
     }
 
